809fa4fc9cea32676abfcc8185ccec7afc53c7a7
Fer Wicker <fer.wicker@gmail.com>
2021-05-06 20:21:04 +1000

    setting up server mongo connection

--- server.js
+++ server.js
@@ -1,12 +1,29 @@
 const express = require("express");
+const mongoose = require("mongoose");
 const path = require("path");
+
 const PORT = process.env.PORT || 3001;
+
 const app = express();
+
+app.use(express.urlencoded({ extended: true }));
+app.use(express.json());
 
 // Serve up static assets (usually on heroku)
 if (process.env.NODE_ENV === "production") {
   app.use(express.static("client/build"));
 }
+
+mongoose.connect(
+  process.env.MONGODB_URI || "mongodb://localhost/user", {
+    useNewUrlParser: true,
+    useUnifiedTopology: true,
+    useCreateIndex: true,
+    useFindAndModify: false
+});
+
+// routes
+require("./routes/api.js")(app);
 
 // Send every request to the React app
 // Define any API routes before this runs
